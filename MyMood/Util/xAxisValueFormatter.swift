2e02fa63238d85eca883aab6ac49a3396dedf70f
Glauber Gustavo <gustavo.glauber@gmail.com>
2023-04-10 23:38:44 -0300

    fix file .swift

---
+++ MyMood/Util/xAxisValueFormatter.swift
@@ -0,0 +1,39 @@
+//
+//  xAxisValueFormatter.swift
+//  MyMood
+//
+//  Created by Glauber Gustavo on 10/04/23.
+//
+
+import Foundation
+import Charts
+
+class xAxisValueFormatter: AxisValueFormatter {
+    
+    var formattedDates: [String] = []
+    var date: [Double] = []
+    
+    init(formattedDates: [String], date: [Double]) {
+        self.formattedDates = formattedDates
+        self.date = date
+    }
+    
+    func findIndex( list: [Double],  value: Double) -> Int? {
+        for (index, item) in list.enumerated() {
+            if item == value {
+                return index
+            }
+        }
+        return nil
+    }
+    
+    func stringForValue(_ value: Double, axis: Charts.AxisBase?) -> String {
+        if date.contains(value) {
+            return formattedDates[findIndex(list: date, value: value) ?? 0]
+        }
+        
+        return ""
+    }
+    
+    
+}
